6a1843a752042606be9251a6a0bc66cad1d2cd52
JOE <CreekJS@cardiff.ac.uk>
2018-10-09 15:53:00 +0100

    i don't know what i'm doing

--- hello.py
+++ hello.py
@@ -1,2 +1,4 @@
 print("hello there, time to .git GUD suckas")
 print("this is getting out of hand, now there are two of them")
+print("this line didn't change")
+print("4th line because it said i had to")
